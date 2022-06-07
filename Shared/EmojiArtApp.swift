58f2e4d9bdab66c639391d35026a16489885964f
LIKANG <79990647+SAKURA-CAT@users.noreply.github.com>
2022-06-07 21:00:39 +0800

    Initial Commit

---
+++ Shared/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  Shared
+//
+//  Created by 李抗 on 2022/6/7.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
